e8d743731e24e7836a747f608e45ad94c455836b
Mauve3 <mauvemoonman@gmail.com>
2016-05-02 16:13:12 -0700

    for save

--- assets/js/directives/se-ad.js
+++ assets/js/directives/se-ad.js
@@ -1,9 +1,21 @@
-angular.module('ambrosia').directive('seAd', [ function () {
+angular.module('ambrosia').directive('seAd', ['$templateCache', function ($templateCache) {
   return {
     restrict: 'E',
-    templateUrl: '/assets/html/misc/ad_google.html',
+    templateUrl: function(){
+        //This is kind of a cheat to figure out if ads are blocked
+        //A variable is assigned in showads.js. If it's undefined,
+        //we know we have something blocking our ads
+
+        if( window.canRunAds === undefined ){
+          console.log('blocked')
+          return '/assets/html/misc/alt_no_ad.html'
+        } else {
+          console.log('not blocked')
+          return '/assets/html/misc/ad_google.html'
+        }
+    },
     controller: function(){
-        (adsbygoogle = window.adsbygoogle || []).push({});
+        (adsbygoogle = window.adsbygoogle || []).push({})
     }
   }
 }])
